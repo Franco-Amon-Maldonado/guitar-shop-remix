244256e90cbe02105c486fce7ab5cdf6df823da0
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-03-29 10:56:02 -0300

    29/03/2023
    -Se creo componente listado-guitarras
    
    -Se hace uso de loader con "promises.all" en el componente index, para hacer multiples llamadas a la api
    
    -Se elimina el map de guitarras en tienda.jsx y se lo modulariza en un componente

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,45 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/api/guitarras.server";
+import { getPots } from "~/api/posts.server";
+import ListadoGuitarras from "~/components/listado-guitarras";
+
+export function meta(){
+
+}
+
+export function links(){
+    
+}
+
+export async function loader(){
+
+    const [guitarras, posts] = await Promise.all([
+        getGuitarras(),
+        getPots()
+    ])
+
+    //Esto de abajo se reemplazo por lo de Promise.all
+    // const guitarras = await getGuitarras();
+    // const posts = await getPots();
+
+    return {guitarras: guitarras.data, posts: posts.data}
+}
+
+
+
+
 function Index() {
+    const {guitarras, posts} = useLoaderData()
+    console.log(guitarras)
     return ( 
-        <div>
-            index.jsx
-        </div> 
+        <>
+        <main className="contenedor">
+            <ListadoGuitarras
+                guitarras={guitarras}
+            />
+        </main>
+        
+        </>
     );
 }
 
