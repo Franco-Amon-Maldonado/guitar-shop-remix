ca3ed2bbd2898ff1f940ca8d64d428d3463cd0a3
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-04-14 00:32:58 -0300

    14/04/2023
    -En root se pasa el carrito por props por context
    
    -En el componente carrito se obtiene lo del carrito y se muestra por pantalla
    
    -Se le da estilos al componente carrito

--- app/root.jsx
+++ app/root.jsx
@@ -68,7 +68,8 @@ export default function App(){
         <Document>
             <Outlet
                 context={{
-                    agregarCarrito
+                    agregarCarrito,
+                    carrito
                 }}
             />
         </Document>
--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,4 +1,6 @@
+import { useOutletContext } from '@remix-run/react'
 import styles from '~/styles/carrito.css'
+
 
 
 export function meta(){
@@ -18,6 +20,8 @@ export function links(){
 }
 
 function Carrito() {
+    const {carrito} = useOutletContext()
+
     return ( 
       <main className="contenedor">
         <h1 className="heading">
@@ -27,11 +31,28 @@ function Carrito() {
         <div className="contenido">
             <div className="carrito">
                 <h2>Articulos</h2>
+                {carrito.length === 0 ? 'Carrito Vacio' : (
+                    carrito.map( producto => (
+                        <div key={producto.id} className="producto">
+                            <div>
+                                <img src={producto.imagen} alt="imagen de guitarra" />
+                            </div>
+                            <div className='informacion'>
+                                <p className="nombre">{producto.nombre}</p>
+                                <div className='precio-final'>
+                                    <p className="precio">Precio c/u: $<span>{producto.precio}</span></p>
+                                    <p className="subtotal">Subtotal: $<span>{producto.precio*producto.cantidad}</span></p>
+                                </div>  
+                            </div>
+                            
+                        </div>
+                    ))
+                )}
             </div>
 
             <aside className="resumen">
                 <h3>Resumen Pedido</h3>
-                <p>Total a pagar</p>
+                <p>Total a pagar: $</p>
             </aside>
         </div>
 
